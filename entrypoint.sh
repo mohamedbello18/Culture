a2093ce089c8956bf180044f4c0cf61a4bddbf20
BELLO Mohamed <mohamedbello717@gmail.com>
2025-12-14 11:04:15 +0100

    Resolving <<  Unable to prepare route [confirm-password] for serialization. Another route has already been assigned name [password.confirm]. >>  errors

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,23 +2,16 @@
 set -e
 
 # Set correct permissions. This is important because the storage volume is mounted at runtime.
-chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chown -R www-data:www-data /var/www/html/storage /var/w/html/bootstrap/cache
 chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
 
 echo "🚀 Running deployment tasks..."
 
-# Run database migrations
-php artisan migrate --force
-
-# Clear and optimize caches
+# Clear caches (without caching)
 php artisan config:clear
 php artisan cache:clear
 php artisan view:clear
 php artisan route:clear
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
-php artisan event:cache
 
 echo "✅ Deployment tasks completed. Starting server."
 
