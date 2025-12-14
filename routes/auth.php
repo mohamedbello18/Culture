0d9196c25f07e2ee8af106226a1b980106094782
BELLO Mohamed <mohamedbello717@gmail.com>
2025-12-14 12:31:29 +0100

    Correction

--- routes/auth.php
+++ routes/auth.php
@@ -1,3 +1,4 @@
+
 <?php
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
@@ -10,6 +11,7 @@ use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
+/*
 
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
@@ -47,6 +49,8 @@ Route::middleware('auth')->group(function () {
         ->middleware('throttle:6,1')
         ->name('verification.send');
 
+    Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
+        ->name('password.confirm');
 
     //Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
 
